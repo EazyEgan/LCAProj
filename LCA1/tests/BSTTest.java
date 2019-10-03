e6d2bdac57fdb71d604263b450155f9764d08e76
Liam Egan <liamegann@gmail.com>
2019-10-03 14:20:00 +0000

    t Function names added

---
+++ LCA1/tests/BSTTest.java
@@ -0,0 +1,44 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+//-------------------------------------------------------------------------
+
+@RunWith(JUnit4.class)
+public class BSTTest
+{
+
+    //TODO write more tests here.
+
+
+
+    @Test
+    public void testheight() {
+    }
+
+@Test
+public void testMedian() {
+
+
+
+@Test
+public void testPrintKeysInOrder() {
+
+}
+
+
+    @Test
+    public void testPrettyPrint() {
+
+    }
+
+
+    
+    @Test
+    public void testDelete() {
+
+    }
+
+}
