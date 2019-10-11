d09bac097a3d24c775ac2157836d4e8204f83a29
Liam Egan <liamegann@gmail.com>
2019-10-11 17:17:00 +0000

    dded Test Function names to DAGTEST

---
+++ LCA1/tests/DAGTest.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+@RunWith(JUnit4.class)
+public class BSTTest
+{
+    @Test
+    public void testAddEdge(){
+
+    }
+
+    @Test
+    public void testV(){
+
+    }
+
+    @Test
+    public void testAdj(){
+
+    }
+
+    //	@Test
+    public void testDagLowestCommonAncestor(){
+        
+    }
+}
